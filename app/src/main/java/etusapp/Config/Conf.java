ba7fe7934c6da10ec9b10b7ea59d70743c6a6c9d
chira <chiraz231996@gmail.com>
2019-10-08 05:18:35 +0200

    initial commit

---
+++ app/src/main/java/etusapp/Config/Conf.java
@@ -0,0 +1,13 @@
+package etusapp.Config;
+
+/**
+ * Created by DELL on 16/06/2018.
+ */
+
+public class Conf {
+
+    public static final String getStation = "https://chirazch.000webhostapp.com/get_station.php";
+    public static final String arrets="https://chirazch.000webhostapp.com/arret.php";
+    public static final String ServerURL = "https://chirazch.000webhostapp.com/get_data.php" ;
+
+}
